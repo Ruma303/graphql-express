72e71e5eb6d59986ec96e9889ba3617b5f1513db
Ruma303 <matteoh8@hotmail.it>
2024-05-26 11:04:47 +0200

    ok

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,5 +1,7 @@
 const users = require('../database/Users.json');
 const posts = require('../database/Posts.json');
+const fs = require('fs');
+const path = require('path');
 
 const {
     GraphQLObjectType,
@@ -7,7 +9,8 @@ const {
     GraphQLString,
     GraphQLSchema,
     GraphQLList,
-    GraphQLEnumType
+    GraphQLNonNull,
+    GraphQLEnumType,
 } = require("graphql");
 
 
@@ -86,110 +89,180 @@ const RootQuery = new GraphQLObjectType({
 });
 
 
-    //! modifica i resolvers per utilizzare JavaScript
-    //% Mutations
-    const RootMutation = new GraphQLObjectType({
-        name: 'RootMutation',
-        fields: {
-            addUser: {
-                type: UserType,
-                args: {
-                    id: { type: GraphQLID },
-                    name: { type: GraphQLString },
-                    email: { type: GraphQLString },
-                    phone: { type: GraphQLString }
-                },
-                resolve: async (parent, args) => {
-                    const user = await User.create({
-                        name: args.name,
-                        email: args.email,
-                        phone: args.phone
-                    });
-                    return user;
-                }
+//% Mutations
+const RootMutation = new GraphQLObjectType({
+    name: 'RootMutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                email: { type: new GraphQLNonNull(GraphQLString) },
+                password: { type: new GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLString }
             },
-            updateUser: {
-                type: UserType,
-                args: {
-                    id: { type: GraphQLID },
-                    name: { type: GraphQLString },
-                    email: { type: GraphQLString },
-                    phone: { type: GraphQLString }
-                },
-                resolve: async (parent, args) => {
-                    const user = await User.findByIdAndUpdate(args.id, args, { new: true });
-                    return user;
-                }
-            },
-            deleteUser: {
-                type: UserType,
-                args: {
-                    id: { type: GraphQLID }
-                },
-                resolve: async (parent, args) => {
-                    const user = await User.findByIdAndDelete(args.id);
-                    return user;
-                }
-            },
-            addPost: {
-                type: PostType,
-                args: {
-                    id: { type: GraphQLID },
-                    userId: { type: GraphQLID },
-                    name: { type: GraphQLString },
-                    description: { type: GraphQLString },
-                    status: {
-                        type: new GraphQLEnumType({
-                            name: 'PostStatus',
-                            values: {
-                                PENDING: { value: 'pending' },
-                                COMPLETED: { value: 'completed' },
-                                CANCELLED: { value: 'cancelled' }
-                            }
-                        }),
-                        defaultValue: 'pending'
-                    },
-                    userId: { type: GraphQLID }
-                },
-                resolve: async (parent, args) => {
-                    const post = await Post.create({
-                        userId: args.userId,
-                        name: args.name,
-                        description: args.description,
-                        status: args.status
-                    });
-                    return post;
-                }
-            },
-            updatePost: {
-                type: PostType,
-                args: {
-                    id: { type: GraphQLID },
-                    userId: { type: GraphQLID },
-                    name: { type: GraphQLString },
-                    description: { type: GraphQLString },
-                    status: { type: GraphQLString }
-                },
-                resolve: async (parent, args) => {
-                    const post = await Post.findByIdAndUpdate(args.id, args, { new: true });
-                    return post;
-                }
-            },
-            deletePost: {
-                type: PostType,
-                args: {
-                    id: { type: GraphQLID }
-                },
-                resolve: async (parent, args) => {
-                    const post = await Post.findByIdAndDelete(args.id);
-                    return post;
-                }
+            resolve: (parent, args) => {
+                const newUser = {
+                    id: args.id || Date.now().toString(),
+                    name: args.name,
+                    email: args.email,
+                    password: args.password,
+                    phone: args.phone
+                };
+                users.push(newUser);
+                fs.writeFile(
+                    path.join(__dirname, '../database/Users.json'),
+                    JSON.stringify(users, null, 2),
+                    err => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        }
+                    }
+                );
+                return newUser;
             }
-        }
-    });
+        },
+        updateUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                email: { type: new GraphQLNonNull(GraphQLString) },
+                password: { type: new GraphQLNonNull(GraphQLString) },
+                phone: { type: GraphQLString }
+            },
+            resolve: (parent, args) => {
+                const user = users.find(user => user.id === args.id);
+                if (!user) {
+                    throw new Error('User not found');
+                }
+                Object.keys(args).forEach(key => {
+                    if (args[key] !== undefined) {
+                        user[key] = args[key];
+                    }
+                });
+                fs.writeFile(
+                    path.join(__dirname, '../database/Users.json'),
+                    JSON.stringify(users, null, 2),
+                    err => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        }
+                    }
+                );
+                return user;
+            }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) }
+            },
+            resolve: (parent, args) => {
+                const index = users.findIndex(user => user.id === args.id);
+                if (index === -1) {
+                    throw new Error('User not found');
+                }
+                const deletedUsers = users.splice(index, 1);
+                fs.writeFile(
+                    path.join(__dirname, '../database/Users.json'),
+                    JSON.stringify(users, null, 2),
+                    err => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        }
+                    }
+                );
+                return deletedUsers[0];
+            }
+        },
+        addPost: {
+            type: PostType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                userId: { type: new GraphQLNonNull(GraphQLID) },
+                title: { type: new GraphQLNonNull(GraphQLString) },
+                description: { type: GraphQLString }
+            },
+            resolve: (parent, args) => {
+                const newPost = {
+                    id: args.id || Date.now().toString(),
+                    userId: args.userId,
+                    title: args.title,
+                    description: args.description
+                };
+                posts.push(newPost);
+                fs.writeFile(
+                    path.join(__dirname, '../database/Posts.json'),
+                    JSON.stringify(posts, null, 2),
+                    err => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        }
+                    }
+                );
+                return newPost;
+            }
+        },
+        updatePost: {
+            type: PostType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                userId: { type: GraphQLID },
+                title: { type: GraphQLString },
+                description: { type: GraphQLString }
+            },
+            resolve: (parent, args) => {
+                const post = posts.find(post => post.id === args.id);
+                if (!post) {
+                    throw new Error('Post not found');
+                }
+                Object.keys(args).forEach(key => {
+                    if (args[key] !== undefined) {
+                        post[key] = args[key];
+                    }
+                });
+                fs.writeFile(
+                    path.join(__dirname, '../database/Posts.json'),
+                    JSON.stringify(posts, null, 2),
+                    err => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        }
+                    }
+                );
+                return post;
+            }
+        },
+        deletePost: {
+            type: PostType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) }
+            },
+            resolve: (parent, args) => {
+                const index = posts.findIndex(post => post.id === args.id);
+                if (index === -1) {
+                    throw new Error('Post not found');
+                }
+                const deletedPosts = posts.splice(index, 1);
+                fs.writeFile(
+                    path.join(__dirname, '../database/Posts.json'),
+                    JSON.stringify(posts, null, 2),
+                    err => {
+                        if (err) {
+                            console.error('Error writing file:', err);
+                        }
+                    }
+                );
+                return deletedPosts[0];
+            }
+        },
+    }
+});
 
 
-    module.exports = new GraphQLSchema({
-        query: RootQuery,
-        mutation: RootMutation
-    });
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    mutation: RootMutation
+});
